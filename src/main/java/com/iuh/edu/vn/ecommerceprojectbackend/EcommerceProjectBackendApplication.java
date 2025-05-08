7f0c0773685ca28a430848eb7926557aa47d5dd5
hyuga127 <tranhuyzaza@gmail.com>
2025-05-08 16:59:06 +0700

    First commit

---
+++ src/main/java/com/iuh/edu/vn/ecommerceprojectbackend/EcommerceProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.edu.vn.ecommerceprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProjectBackendApplication.class, args);
+    }
+
+}
